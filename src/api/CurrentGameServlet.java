8eaf4ac1750355ba94f5cc295982fba8cb5a1a82
Rayan <rayan.barman@hotmail.com>
2019-12-03 23:40:22 +0100

    in building : Create dynamic table for currentGames

--- src/api/CurrentGameServlet.java
+++ src/api/CurrentGameServlet.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CurrentGameServlet extends HttpServlet {
     // this is our GET /users API providing all the users as a JSON object
@@ -75,6 +77,38 @@ public class CurrentGameServlet extends HttpServlet {
             resp.setCharacterEncoding("UTF-8");
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.getWriter().write(json);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            System.out.println("Get games");
+
+            Path path = Paths.get("./data/temp/currentGames.json");
+            String json = "{\"success\":\"true\", \"data\":";
+
+            if (Files.exists(path)) {
+                json += new String(Files.readAllBytes(path));
+            }
+            else {
+                json += "\"\"";
+            }
+            json += "}";
+
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(json);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            resp.setContentType("text/html");
+            byte[] msgBytes = e.getMessage().getBytes("UTF-8");
+            resp.setContentLength(msgBytes.length);
+            resp.setCharacterEncoding("utf-8");
+            resp.getOutputStream().write(msgBytes);
         }
     }
 }
