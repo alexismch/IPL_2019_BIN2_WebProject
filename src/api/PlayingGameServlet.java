6b31945e7da767535e0e6ce63debdc0625280a7d
Michiels Alexis <alexis.michiels@student.vinci.be>
2019-12-08 22:19:35 +0100

    FINISH HIM

--- src/api/PlayingGameServlet.java
+++ src/api/PlayingGameServlet.java
@@ -131,4 +131,55 @@ public class PlayingGameServlet extends HttpServlet {
             resp.getOutputStream().write(msgBytes);
         }
     }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String nomPartie = req.getParameter("nomPartie");
+            System.out.println("Delete playing game : " + nomPartie);
+
+            Genson genson = new Genson();
+            String json = new String(Files.readAllBytes(Paths.get("./data/temp/playingGames.json")));
+            Map games = genson.deserialize(json, Map.class);
+            final Map[] targetGame = new Map[1];
+            games.forEach((key, value) -> {
+                if (key.equals(nomPartie))
+                    targetGame[0] = (Map) value;
+            });
+            if (targetGame[0] != null) {
+                games.remove(nomPartie);
+                json = genson.serialize(games);
+                System.out.println(json);
+                Files.write(Paths.get("./data/temp/playingGames.json"), json.getBytes());
+            }
+
+            json = new String(Files.readAllBytes(Paths.get("./data/temp/waitingGames.json")));
+            games = genson.deserialize(json, Map.class);
+            games.forEach((key, value) -> {
+                if (key.equals(nomPartie))
+                    targetGame[0] = (Map) value;
+            });
+            if (targetGame[0] != null) {
+                games.remove(nomPartie);
+                json = genson.serialize(games);
+                System.out.println(json);
+                Files.write(Paths.get("./data/temp/waitingGames.json"), json.getBytes());
+            }
+            String return_json = "{\"success\":\"true\"}";
+            System.out.println("JSON returned :" + return_json);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(return_json);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            resp.setContentType("text/html");
+            byte[] msgBytes = e.getMessage().getBytes("UTF-8");
+            resp.setContentLength(msgBytes.length);
+            resp.setCharacterEncoding("utf-8");
+            resp.getOutputStream().write(msgBytes);
+        }
+    }
 }
